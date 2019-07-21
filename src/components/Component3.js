722a5db21f81715571e709afa4a62df7ef9361bf
Junior Dugue <jrdugue@gmail.com>
2019-07-21 10:23:15 -0400

    added spring animation to component1
    added spring animation to component1 to animate fading in from the top

---
+++ src/components/Component3.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {Spring} from "react-spring";
+
+const Component3 = () => {
+  return (
+      // you want to add Spring component after the return, and whatever we want to animate, we have to put inside of the spring
+    // you also want to return props with a arrow function and add a div container with style={props}
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+    >
+      {props => (
+        <div style={props}>
+          <div style={c1Style}>
+            <h2>Component1</h2>
+            <p>
+              Nulla vitae elit libero, a pharetra augue. Integer posuere erat a
+              ante venenatis dapibus posuere velit aliquet. Cras justo odio,
+              dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus
+              ac cursus commodo, tortor mauris condimentum nibh, ut fermentum
+              massa justo sit amet risus. Cras justo odio, dapibus ac facilisis
+              in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur
+              purus sit amet fermentum. Donec id elit non mi porta gravida at
+              eget metus. Cras mattis consectetur purus sit amet fermentum.
+              Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent
+              commodo cursus magna, vel scelerisque nisl consectetur et. Etiam
+              porta sem malesuada magna mollis euismod. Nullam id dolor id nibh
+              ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed
+              consectetur. Vestibulum id ligula porta felis euismod semper.
+              Vestibulum id ligula porta felis euismod semper. Fusce dapibus,
+              tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+              fermentum massa justo sit amet risus. Morbi leo risus, porta ac
+              consectetur ac, vestibulum at eros. Maecenas sed diam eget risus
+              varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus
+              commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+              sit amet risus. Etiam porta sem malesuada magna mollis euismod.
+            </p>
+          </div>
+        </div>
+      )}
+      </Spring>
+  );
+};
+export default Component3;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Component1 from "./components/Component1";
+// import Component2 from "./components/Component2";
+// import Component3 from "./components/Component3";
 import "./styles.scss";
+
 // npm i react-spring to install react=spring
-function App() {
-  return (
-    <div className="App">
-      <Component1 />
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React Spring</h1>
+        <Component1 />
+        {/* <Component2 /> */}
+        {/* <Component3/> */}
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
